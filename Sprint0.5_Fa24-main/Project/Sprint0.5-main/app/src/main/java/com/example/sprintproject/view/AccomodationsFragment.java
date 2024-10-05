6e32fdb5a8fa1aad9cc5ae00711f294af4051ffd
justin5764 <justinzheng9398@gmail.com>
2024-10-05 17:53:42 -0400

    Add AccomodationsFragment.java and DiningFragment.java

---
+++ Sprint0.5_Fa24-main/Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/view/AccomodationsFragment.java
@@ -0,0 +1,23 @@
+package com.example.CS2340FAC_Team41;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AccommodationsFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_accommodation, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // Additional initialization code if needed
+    }
+}
