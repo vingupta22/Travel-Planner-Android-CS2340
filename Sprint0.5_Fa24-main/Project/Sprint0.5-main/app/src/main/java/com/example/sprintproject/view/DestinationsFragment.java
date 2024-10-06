d47c63913f6ceb8200a6eca90fdda5d4cb427145
Dev-Patel18 <143572779+Dev-Patel18@users.noreply.github.com>
2024-10-05 21:27:43 -0400

    Create DestinationsFragment.java

---
+++ Sprint0.5_Fa24-main/Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/view/DestinationsFragment.java
@@ -0,0 +1,24 @@
+package com.example.CS2340FAC_Team41;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DestinationsFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_destinations, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Initialize UI elements, load data, set up listeners here
+    }
+}
