c326e279b4cf40318c63fe5548a9a8bae823596b
Dev-Patel18 <143572779+Dev-Patel18@users.noreply.github.com>
2024-10-26 21:54:47 -0400

    Update Destination.java

--- Sprint0.5_Fa24-main/Project/Sprint0.5-main/app/src/main/java/model/Destination.java
+++ Sprint0.5_Fa24-main/Project/Sprint0.5-main/app/src/main/java/model/Destination.java
@@ -1,5 +1,39 @@
 package model;
 
 public class Destination {
-    // A destination class to be implemented in a future sprint
+    // The single instance of the class
+    private static Destination instance;
+
+    // Private constructor to prevent instantiation from other classes
+    private Destination() {
+        // Initialization logic if required
+    }
+
+    // Public static method to provide access to the single instance
+    public static synchronized Destination getInstance() {
+        if (instance == null) {
+            instance = new Destination();
+        }
+        return instance;
+    }
+
+    // Example fields and methods (you can extend this as needed)
+    private String name;
+    private int daysPlanned;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDaysPlanned() {
+        return daysPlanned;
+    }
+
+    public void setDaysPlanned(int daysPlanned) {
+        this.daysPlanned = daysPlanned;
+    }
 }
