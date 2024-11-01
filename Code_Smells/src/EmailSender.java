57df9efe8218ca21ebc7d9d7c57ff82b41801671
Dev-Patel18 <143572779+Dev-Patel18@users.noreply.github.com>
2024-11-01 14:44:21 -0400

    Update EmailSender.java

--- Code_Smells/src/EmailSender.java
+++ Code_Smells/src/EmailSender.java
@@ -1,6 +1,5 @@
-
 public class EmailSender {
-    public static void sendEmail(String customerEmail, String subject, String message){
+    public void sendEmail(String customerEmail, String subject, String message){
         System.out.println("Email to: " + customerEmail);
         System.out.println("Subject: " + subject);
         System.out.println("Body: " + message);
