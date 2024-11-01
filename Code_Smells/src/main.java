c76146f70d8fcfeeb62ad92280dbc5847ecec7fa
Dev-Patel18 <143572779+Dev-Patel18@users.noreply.github.com>
2024-11-01 14:48:07 -0400

    Update main.java

--- Code_Smells/src/main.java
+++ Code_Smells/src/main.java
@@ -1,8 +1,13 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class main {
-	public static void main(String[] args) {
+public class Main {
+    public static void main(String[] args) {
+        
+        EmailSender emailSender = new EmailSender();
+        NotificationService notificationService = new NotificationService(emailSender);
+        PriceCalculator priceCalculator = new PriceCalculator();
+
         Item item1 = new Item("Book", 20, 1, DiscountType.AMOUNT, 5);
         Item item2 = new TaxableItem("Laptop", 1000, 1, DiscountType.PERCENTAGE, 0.1);
         Item item3 = new GiftCardItem("Gift Card", 10, 1, DiscountType.AMOUNT, 0);
@@ -12,13 +17,11 @@ public class main {
         items.add(item2);
         items.add(item3);
 
-        Order order = new Order(items, "John Doe", "johndoe@example.com");
+        Order order = new Order(items, "John Doe", "johndoe@example.com", priceCalculator, notificationService);
 
-        System.out.println("Total Price: " + order.calculateTotalPrice());
-
+        System.out.println("Total Price: " + order.getTotalPrice());
         order.sendConfirmationEmail();
 
         order.printOrder();
     }
 }
-
